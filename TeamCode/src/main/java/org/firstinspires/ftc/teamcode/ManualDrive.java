0275582fbfcd139a6593253f03d4fbd4c8a1f206
fots18535 <fots18535@gmail.com>
2021-10-17 16:30:09 -0700

    added teleop

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ManualDrive.java
@@ -0,0 +1,112 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class ManualDrive extends LinearOpMode
+{
+    DcMotor leftBack;
+    DcMotor leftFront;
+    DcMotor rightBack;
+    DcMotor rightFront;
+    DcMotor turnTable;
+    DcMotor gandalfStaff;
+    Servo clampy;
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        leftBack = hardwareMap.get(DcMotor.class, "leftBack");
+        leftFront = hardwareMap.get(DcMotor.class, "leftFront");
+        rightBack = hardwareMap.get(DcMotor.class, "rightBack");
+        rightFront = hardwareMap.get(DcMotor.class, "rightFront");
+        turnTable = hardwareMap. get(DcMotor.class. "duckyTable");
+        gandalfStaff = hardwareMap.get(DcMotor.class, "spinnyArmOfDeath");
+        clampy = hardwareMap.get(Servo.class, "clampyArmOfDeath");
+
+
+        // Stops coasting
+        leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+
+        waitForStart();
+
+
+        while (opModeIsActive()) {
+            //Get the input from the gamepad controller
+            double leftX =   gamepad1.left_stick_x;
+            double leftY =   gamepad1.left_stick_y;
+            double rightX =  -gamepad1.right_stick_x;
+            double rightY =  gamepad1.right_stick_y;
+
+            //manually controls turntable
+            boolean leftPad = gamepad1. dpad_left;
+            boolean rightPad = gamepad1. dpad_left;
+
+            if(leftPad){
+               turnTable. setPower(1);
+            }else if(rightPad){
+                turnTable. setPower(-1);
+            }else{
+                turnTable. setPower(0);
+            }
+
+
+            //manually controls gandalf's arm
+            boolean upPad = gamepad1. dpad_left;
+            boolean downPad = gamepad1. dpad_left;
+
+            if(upPad){
+                gandalfStaff. setPower(0.5);
+            }else if(downPad){
+                gandalfStaff. setPower(-0.5);
+            }else{
+                gandalfStaff.setPower(0);
+            }
+
+            //set places for gandalf's arm
+                //button y = 1 (ground)
+                //button b = 2 (first level)
+                //button a = 3 (second level)
+                //button x = 4 (third level)
+                //right bumper = 5 (topper)
+
+            boolean buttonY = gamepad2. y;
+            boolean buttonB = gamepad2. b;
+            boolean buttonA = gamepad2. a;
+            boolean buttonX = gamepad2. x;
+            boolean rightBumper = gamepad2. right_bumper;
+
+            if(buttonY){
+
+                        //TODO:Encoder stuff
+            }
+
+            //manually controls clampy
+            float rightTrigger = gamepad2. right_trigger;
+
+
+                clampy. setPosition (rightTrigger);
+
+
+                // Setting the motor power based on the input
+            leftBack.setPower(rightX + rightY + leftX);
+            leftFront.setPower(rightX + rightY - leftX);
+            rightBack.setPower(rightX - rightY + leftX);
+            rightFront.setPower(rightX - rightY - leftX);
+
+
+        }
+
+        // Stop the motors
+        leftBack.setPower(0);
+        leftFront.setPower(0);
+        rightBack.setPower(0);
+        rightFront.setPower(0);
+
+    }
+}
