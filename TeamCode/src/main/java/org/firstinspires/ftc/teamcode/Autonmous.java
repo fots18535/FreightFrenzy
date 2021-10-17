4276af7fe71999f59072412d02cd0a4e30b4a59a
Andy Fisher <afisher2@users.noreply.github.com>
2021-10-16 20:56:39 -0500

    git

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonmous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonmous.java
@@ -10,5 +10,18 @@ public class Autonmous extends LinearOpMode {
         // GO TGO DUCK
         // stan lana del rey
         //
+
+        HunkOfMetal hunk = new HunkOfMetal(this);
+        hunk.initialize();
+        waitForStart();
+
+        // SCAN YUHt
+
+        hunk.chaChaRealSmooth(1, 24);
+
+        //  SPINM DUCK
+
+
+
     }
 }
